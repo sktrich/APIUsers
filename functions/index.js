81fad4c696a1840bcc1e5d5362182ac6316699a6
sktrich <r.arumakan@gmail.com>
2025-02-02 22:04:52 -0300

    esquecee

--- functions/index.js
+++ functions/index.js
@@ -12,9 +12,9 @@ const logger = require("firebase-functions/logger");
 const express = require("express");
 const cors = require("cors");
 
-const functions = require("firebase-functions")
-const { PrismaClient } = require('@prisma/client')
-const prisma = new PrismaClient()
+const functions = require("firebase-functions");
+const {PrismaClient} = require("@prisma/client");
+const prisma = new PrismaClient();
 // use `prisma` in your application to read and write data in your DB
 
 // Create and deploy your first functions
@@ -27,29 +27,24 @@ exports.helloWorld = onRequest((request, response) => {
 
 // console.log('DATABASE_URL:', process.env.DATABASE_URL)
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-//endereço do frontEnd que vai acessar
-app.use(cors('http://localhost:5173'))
+app.use(express.json());
+// endereço do frontEnd que vai acessar
+app.use(cors("http://localhost:5173"));
 
-app.post('/users', async (req, res) =>{
-
+app.post("/users", async (req, res) =>{
     await prisma.user.create({
-        data:{
+        data: {
             email: req.body.email,
             name:  req.body.name,
             age: req.body.age
         }
     })
-
     res.status(201).json(req.body)
-
-})
-
+});
 
 app.get('/users', async (req, res) =>{
-
     let users = []
     if (Object.keys(req.query).length> 0) {
 
@@ -73,28 +68,17 @@ app.get('/users', async (req, res) =>{
          users = await prisma.user.findMany({
             where: filters
         });
-        // users = await prisma.user.findMany({
-        //     where:{
-        //         name: req.query.name,
-        //         age: parseInt(req.query.age,10),
-        //         email: req.query.email
-        //     }
-        // })
     } else {
         users = await prisma.user.findMany()
     }
-
     res.status(200).json(users)
 })
 
 /* :id variavel criada para pegar id da requisição */
 app.put('/users/:id', async (req, res) =>{
-
     await prisma.user.update({        
-        
         where:{
             id: req.params.id
-
         },
         data:{
             email: req.body.email,
@@ -102,25 +86,16 @@ app.put('/users/:id', async (req, res) =>{
             age: req.body.age
         }
     })
-
     res.status(201).json(req.body)
-
 })
 
-
-
 app.delete('/users/:id', async (req, res) =>{
-
     await prisma.user.delete({        
-        
         where:{
             id: req.params.id
-
         },
     })
-
     res.status(200).json({message: "Usuário deletado com Sucesso"})
-
 })
 
 exports.app = functions.https.onRequest(app);
